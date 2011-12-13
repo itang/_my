bc49bcfa3a38ac1e9dd87d9ca6c67b491829e7e3
itang <live.tang@gmail.com>
2011-12-13 20:21:11 +0800

    update midje version

--- learn/clojure/prog-clojure/project.clj
+++ learn/clojure/prog-clojure/project.clj
@@ -1,4 +1,4 @@
 (defproject prog-clojure "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[midje "1.3.0-RC4"] [lein-midje "1.0.7"]])
+  :dev-dependencies [[midje "1.3.0"] [lein-midje "1.0.7"]])
